75140b5fbe3c9c997d7cb9aac431d9fe78a0aee2
Brady Henderson <118471975+bradyhendu@users.noreply.github.com>
2023-11-27 18:49:56 -0500

    Sets up the frame

--- Yahtzee.java
+++ Yahtzee.java
@@ -2,7 +2,30 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Yahtzee extends jFrame{
+
+    public Yahtzee() {
+        // Create a new JFrame
+        JFrame frame = new JFrame("Yahtzee");
+
+        // Set the size of the frame
+        frame.setSize(800, 600);
+
+        //not resizeable
+        frame.setResizable(false);
+
+        // Set the frame to close when the user clicks the X button
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create a new JPanel
+        JPanel panel = new JPanel();
+
+        // Add the panel to the frame
+        frame.add(panel);
+
+        // Make the frame visible
+        frame.setVisible(true);
+    }
     public static void main(String[] args) {
-        
+        new Yahtzee();
     }
 }
